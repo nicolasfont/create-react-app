ebf5fb0cf2e17a7ee5141c781b8fbc8141b3db07
Nicolás Font <nicolasfont@gmail.com>
2025-10-20 11:36:04 -0300

    Use @tsconfig/node24

--- index.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-import { downloadTemplate } from "giget";
-
-console.log("Creating React App");
-
-const { source, dir } = await downloadTemplate("github:nicolasfont/react-app-template");
-
-console.log(`Created app from ${source} located at ${dir}`);
---
+++ src/index.ts
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+import { downloadTemplate } from "giget";
+
+const appName = process.argv[2] || "react-app";
+
+console.log(`Downloading template`);
+
+await downloadTemplate("github:nicolasfont/react-app-template", { dir: appName });
+
+console.log(`Created app ${appName}`);
+
