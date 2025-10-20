49317063c3ad35c9039d1ea79887df547cf0af66
Nicolás Font <nicolasfont@gmail.com>
2025-10-20 17:29:59 -0300

    Replace template name with given app name

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 import { downloadTemplate } from "giget";
+import { replaceInFile } from "replace-in-file";
 
 const appName = process.argv[2] || "react-app";
 
@@ -7,6 +8,12 @@ console.log("Downloading template");
 
 await downloadTemplate("github:nicolasfont/react-app-template", {
   dir: appName,
+});
+
+await replaceInFile({
+  files: `${appName}/**/*`,
+  from: /react-app-template/g,
+  to: appName,
 });
 
 console.log(`Created ${appName}`);
